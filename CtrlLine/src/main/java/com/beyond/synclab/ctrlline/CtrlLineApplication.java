6faf81ab167154cb49a6d0329518603a8e0ee7f1
서현원 <109474853+viroovr@users.noreply.github.com>
2025-10-27 10:30:51 +0900

    [Feat] init 백엔드 서버 (#8)
    * [Chore] 깃 이그노어 설정
    
    * [Feat] mes와 erp 자바 서버 생성
    
    * [Feat] application yaml로 변경

---
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/CtrlLineApplication.java
@@ -0,0 +1,13 @@
+package com.beyond.synclab.ctrlline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtrlLineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CtrlLineApplication.class, args);
+    }
+
+}
---
+++ CtrlLine/src/test/java/com/beyond/synclab/ctrlline/CtrlLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beyond.synclab.ctrlline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtrlLineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
