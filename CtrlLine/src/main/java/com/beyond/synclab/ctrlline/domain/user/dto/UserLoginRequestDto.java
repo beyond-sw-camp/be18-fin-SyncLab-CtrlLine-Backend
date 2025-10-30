9bc90277bff89ebab531dc0b34ade6e984131f0b
viroovr <tjgusdnjs789@gmail.com>
2025-10-30 16:32:24 +0900

    [Refactor] Dto들 builder로 생성가능하도록 어노테이션 추가

--- CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/user/dto/UserLoginRequestDto.java
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/user/dto/UserLoginRequestDto.java
@@ -1,12 +1,16 @@
 package com.beyond.synclab.ctrlline.domain.user.dto;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Getter
 @ToString
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserLoginRequestDto {
     private String email;
--- CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/user/dto/UserSignupRequestDto.java
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/user/dto/UserSignupRequestDto.java
@@ -6,14 +6,17 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserSignupRequestDto {
     @NotBlank(message = "이름은 필수입니다.")
     private String name;
