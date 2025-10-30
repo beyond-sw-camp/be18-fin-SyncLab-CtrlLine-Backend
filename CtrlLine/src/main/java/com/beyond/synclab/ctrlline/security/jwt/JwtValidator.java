1abbfcbdc69bd734edc2a86dfe2c453454c74275
viroovr <tjgusdnjs789@gmail.com>
2025-10-30 16:29:38 +0900

    [Feat] JwtValidator 제거 JwtStoreServiceImpl로 통합

--- CtrlLine/src/main/java/com/beyond/synclab/ctrlline/security/jwt/JwtValidator.java
+++
@@ -1,8 +0,0 @@
-package com.beyond.synclab.ctrlline.security.jwt;
-
-import com.beyond.synclab.ctrlline.domain.user.service.CustomUserDetails;
-
-public interface JwtValidator {
-    boolean isBlacklisted(String email);
-    CustomUserDetails getUserDetails(String email);
-}
