461fa05fe9375cdbfffa38a52553e925d5456b70
viroovr <tjgusdnjs789@gmail.com>
2025-11-04 11:24:50 +0900

    [Feat] 유저 회원가입 완료 DTO 필드 변경

--- CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/user/dto/UserSignupResponseDto.java
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/user/dto/UserSignupResponseDto.java
@@ -1,6 +1,7 @@
 package com.beyond.synclab.ctrlline.domain.user.dto;
 
 import com.beyond.synclab.ctrlline.domain.user.entity.Users;
+import com.beyond.synclab.ctrlline.domain.user.entity.Users.UserRole;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -10,15 +11,17 @@ import lombok.Getter;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
 public class UserSignupResponseDto {
-    private final Long id;
     private final String email;
     private final String name;
+    private final String empNo;
+    private final UserRole role;
 
     public static UserSignupResponseDto fromEntity(Users user) {
         return UserSignupResponseDto.builder()
-                .id(user.getId())
                 .email(user.getEmail())
                 .name(user.getName())
+                .empNo(user.getEmpNo())
+                .role(user.getRole())
                 .build();
     }
 }
