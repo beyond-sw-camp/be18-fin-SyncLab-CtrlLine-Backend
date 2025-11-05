2dc6e3f28fe0d58957aba238fd7d684fec14e030
viroovr <tjgusdnjs789@gmail.com>
2025-11-05 12:40:31 +0900

    [Refactor] AuditorAware 이름변경

--- CtrlLine/src/main/java/com/beyond/synclab/ctrlline/config/AuditorAwareImpl.java
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/config/AuditorAwareImpl.java
@@ -8,7 +8,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
-public class AuditorAwareConfig implements AuditorAware<Long> {
+public class AuditorAwareImpl implements AuditorAware<Long> {
 
     @Override
     public Optional<Long> getCurrentAuditor() {
