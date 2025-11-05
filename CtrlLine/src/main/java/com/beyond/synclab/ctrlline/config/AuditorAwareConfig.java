d8c5e4780d39a9a1003edd7ba8e7f95b34a9eaa7
viroovr <tjgusdnjs789@gmail.com>
2025-11-05 10:30:35 +0900

    [Feat] AuditorAwareConfig 작성

---
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/config/AuditorAwareConfig.java
@@ -0,0 +1,27 @@
+package com.beyond.synclab.ctrlline.config;
+
+import com.beyond.synclab.ctrlline.domain.user.service.CustomUserDetails;
+import java.util.Optional;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@Configuration
+public class AuditorAwareConfig implements AuditorAware<Long> {
+
+    @Override
+    public Optional<Long> getCurrentAuditor() {
+        try {
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            if (auth == null || !auth.isAuthenticated() || auth.getPrincipal().equals("anonymousUser")) {
+                return Optional.of(0L);
+            }
+
+            CustomUserDetails userDetails = (CustomUserDetails) auth.getPrincipal();
+            return Optional.ofNullable(userDetails.getId());
+        } catch (Exception e) {
+            return Optional.of(0L);
+        }
+    }
+}
