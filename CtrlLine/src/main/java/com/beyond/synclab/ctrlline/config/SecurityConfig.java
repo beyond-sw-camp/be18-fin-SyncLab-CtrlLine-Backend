a4c6a30fab65330c97b5b56c05d54a5eedc2543a
viroovr <tjgusdnjs789@gmail.com>
2025-11-06 15:24:49 +0900

    [Feat] MethodSecurity 사용설정

--- CtrlLine/src/main/java/com/beyond/synclab/ctrlline/config/SecurityConfig.java
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -26,6 +27,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
     private final UserAuthSuccessHandler userAuthSuccessHandler;
     private final UserAuthFailureHandler userAuthFailureHandler;
@@ -86,7 +88,6 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(AUTH_WHITE_LIST).permitAll()
-                        .requestMatchers("/api/**").hasAnyRole("ADMIN", "MANAGER", "USER")
                         .anyRequest().authenticated()
                 );
 
