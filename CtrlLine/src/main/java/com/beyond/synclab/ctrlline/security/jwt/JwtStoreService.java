8e942485c425343c969b1b4567570dbef41e3fbc
viroovr <tjgusdnjs789@gmail.com>
2025-11-10 11:07:44 +0900

    [Refactor] JwtStore에 액세스토큰 무효화 메소드 등록

--- CtrlLine/src/main/java/com/beyond/synclab/ctrlline/security/jwt/JwtStoreService.java
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/security/jwt/JwtStoreService.java
@@ -7,4 +7,6 @@ public interface JwtStoreService {
     void saveRefreshToken(String email, String refreshToken, long refreshTtl);
     boolean isBlacklisted(String email);
     CustomUserDetails getUserDetails(String email);
+
+    void blacklistAccessToken(String jti, long ttl);
 }
