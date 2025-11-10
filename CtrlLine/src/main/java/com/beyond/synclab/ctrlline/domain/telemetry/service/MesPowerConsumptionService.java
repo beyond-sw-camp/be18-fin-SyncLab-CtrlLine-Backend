3736cb7d7e31974cc1f0238a805df425d6ca7477
dobbyrr <zxr1033@gmail.com>
2025-11-10 16:45:49 +0900

    [Feat] 주석제거

--- CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/telemetry/service/MesPowerConsumptionService.java
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/telemetry/service/MesPowerConsumptionService.java
@@ -10,7 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class MesPowerConsumptionService {
-// rename branch
+
     private final MesDataRepository mesDataRepository;
 
     @Transactional
