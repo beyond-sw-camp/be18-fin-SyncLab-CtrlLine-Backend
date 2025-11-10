69016063f1edf00b00b90238964816fc828bb883
kimeodml <kimeodml@gmail.com>
2025-11-10 15:10:00 +0900

    [Feat] test 관련 application.yaml

--- CtrlLine/src/test/java/com/beyond/synclab/ctrlline/domain/factory/repository/FactoryQueryRepositoryImplTest.java
+++ CtrlLine/src/test/java/com/beyond/synclab/ctrlline/domain/factory/repository/FactoryQueryRepositoryImplTest.java
@@ -15,7 +15,6 @@ import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -23,7 +22,6 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-@ActiveProfiles("test")
 @AutoConfigureMockMvc(addFilters = false)
 @Import(QuerydslTestConfig.class)
 class FactoryQueryRepositoryImplTest {
