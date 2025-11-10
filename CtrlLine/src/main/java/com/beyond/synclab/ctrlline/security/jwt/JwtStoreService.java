26793bc9d579092aad7e03e9901bc8b6d49d354e
viroovr <tjgusdnjs789@gmail.com>
2025-11-10 11:19:59 +0900

    [Refactor] UserRepository 의존성 CustomUserDetailService에 위임

--- CtrlLine/src/main/java/com/beyond/synclab/ctrlline/security/jwt/JwtStoreService.java
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/security/jwt/JwtStoreService.java
@@ -3,10 +3,13 @@ package com.beyond.synclab.ctrlline.security.jwt;
 import com.beyond.synclab.ctrlline.domain.user.service.CustomUserDetails;
 
 public interface JwtStoreService {
-    void deleteRefreshToken(String email);
+
     void saveRefreshToken(String email, String refreshToken, long refreshTtl);
+    String getRefreshToken(String username);
+    void deleteRefreshToken(String email);
+    void blacklistAccessToken(String jti, long ttl);
+
     boolean isBlacklisted(String email);
     CustomUserDetails getUserDetails(String email);
 
-    void blacklistAccessToken(String jti, long ttl);
 }
