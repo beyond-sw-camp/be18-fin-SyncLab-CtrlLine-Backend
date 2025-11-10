62c24c89982ae61d5dbc3d8e5bb0875a3a5365bf
kimeodml <kimeodml@gmail.com>
2025-11-10 10:36:00 +0900

    [Feat] unique 추가로 버전 내용 수정

--- CtrlLine/src/main/resources/db/migration/V1__add_factory_fk_and_unique.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE `factory`
-ADD CONSTRAINT fk_factory_user_id
-FOREIGN KEY (`user_id`)
-REFERENCES `user`(`user_id`)
-ON UPDATE CASCADE;
-
-ALTER TABLE `factory`
-ADD CONSTRAINT uq_factory_code UNIQUE (`factory_code`);
