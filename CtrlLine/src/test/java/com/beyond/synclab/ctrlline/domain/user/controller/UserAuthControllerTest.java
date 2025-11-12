14b0ad3f4b483f60b63715badb0d7b0e5d7b92ed
서현원 <109474853+viroovr@users.noreply.github.com>
2025-11-12 12:07:10 +0900

    [Feat] feat 퇴사한 사용자 로그인 불가능 (#45)
    * [Test] User 로그아웃 성공/실패 테스트 작성
    
    * [Feat] 퇴사 상태일때 권한 부여 실패
    
    * [Test] Auth환경 TestSecuriyConfig 설정
    
    * [Feat] Auth Failure Handler에 퇴사한 사용자 예외 처리 등록
    
    * [Feat] 퇴사한 사용자도 같은 Exception 으로 처리해서 세부내용을 내리지 않지만 퇴사해서 로그인 불가능함을 로그로 확인가능
    
    * [Test] exception 종류 변경

--- CtrlLine/src/test/java/com/beyond/synclab/ctrlline/domain/user/controller/UserAuthControllerTest.java
+++ CtrlLine/src/test/java/com/beyond/synclab/ctrlline/domain/user/controller/UserAuthControllerTest.java
@@ -2,32 +2,36 @@ package com.beyond.synclab.ctrlline.domain.user.controller;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.beyond.synclab.ctrlline.common.exception.AppException;
+import com.beyond.synclab.ctrlline.config.TestSecurityConfig;
 import com.beyond.synclab.ctrlline.domain.user.service.UserAuthService;
-import com.beyond.synclab.ctrlline.security.exception.AuthErrorCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 @WebMvcTest(UserAuthController.class)
-@AutoConfigureMockMvc(addFilters = false)
+@Import(TestSecurityConfig.class)
 class UserAuthControllerTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @TestConfiguration
     static class UserAuthControllerTestContextConfiguration {
         @Bean
@@ -42,6 +46,7 @@ class UserAuthControllerTest {
 
     @Test
     @DisplayName("로그아웃 성공 - 200")
+    @WithMockUser
     void logout_success() throws Exception {
         // given
         doNothing().when(userAuthService).logout(any(HttpServletRequest.class), any(
@@ -57,25 +62,5 @@ class UserAuthControllerTest {
             .andDo(print())
             .andExpect(status().isOk())
             .andExpect(jsonPath("$.code").value(200));
-    }
-
-    @Test
-    @DisplayName("로그아웃 실패 - 401")
-    void logout_unAuthorized() throws Exception {
-        // given
-        doThrow(new AppException(AuthErrorCode.UNAUTHORIZED))
-            .when(userAuthService)
-            .logout(any(HttpServletRequest.class), any(HttpServletResponse.class));
-
-        // when
-        ResultActions resultActions = mockMvc.perform(post("/api/v1/auth/logout")
-            .header("Authorization", "Bearer accessToken")
-            .cookie(new Cookie("refresh_token", "refreshToken")));
-
-        // then
-        resultActions
-            .andDo(print())
-            .andExpect(status().isUnauthorized())
-            .andExpect(jsonPath("$.status").value(401));
     }
 }
