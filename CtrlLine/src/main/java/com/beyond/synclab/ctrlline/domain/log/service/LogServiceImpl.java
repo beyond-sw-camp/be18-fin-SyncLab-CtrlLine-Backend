ea59c9e8d56669e25c2342588b81af3864351ba6
viroovr <tjgusdnjs789@gmail.com>
2025-11-13 11:50:33 +0900

    [Feat] 로그 조회 서비스로직 작성

---
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/log/service/LogServiceImpl.java
@@ -0,0 +1,46 @@
+package com.beyond.synclab.ctrlline.domain.log.service;
+
+import com.beyond.synclab.ctrlline.domain.log.dto.LogCreateRequestDto;
+import com.beyond.synclab.ctrlline.domain.log.dto.LogListResponseDto;
+import com.beyond.synclab.ctrlline.domain.log.dto.LogSearchDto;
+import com.beyond.synclab.ctrlline.domain.log.entity.Logs;
+import com.beyond.synclab.ctrlline.domain.log.repository.LogRepository;
+import com.beyond.synclab.ctrlline.domain.log.spec.LogSpecification;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class LogServiceImpl implements LogService {
+    private final LogRepository logRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<LogListResponseDto> getLogsList(LogSearchDto logSearchDto) {
+        Specification<Logs> spec = Specification.allOf(
+            LogSpecification.logsBetween(logSearchDto.fromDate(), logSearchDto.toDate()),
+            LogSpecification.logsEntityNameEquals(logSearchDto.entityName()),
+            LogSpecification.logsActionTypeEquals(logSearchDto.actionType()),
+            LogSpecification.logsUserIdEquals(logSearchDto.userId())
+        );
+
+        List<Logs> logs = logRepository.findAll(spec, Sort.by(Direction.ASC, "createdAt"));
+
+        return logs.stream().map(LogListResponseDto::fromEntity).toList();
+    }
+
+    @Override
+    @Transactional
+    public void createLog(LogCreateRequestDto logCreateRequestDto) {
+        Logs logs = logCreateRequestDto.toEntity();
+
+        logRepository.save(logs);
+    }
+}
