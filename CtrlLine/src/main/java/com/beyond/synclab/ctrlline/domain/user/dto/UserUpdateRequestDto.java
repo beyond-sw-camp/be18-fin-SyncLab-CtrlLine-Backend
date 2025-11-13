5318095c66ceb7eb8a4379973043ccfd53c5cd07
viroovr <tjgusdnjs789@gmail.com>
2025-11-13 18:39:17 +0900

    [Feat] 업데이트 request에 passaword, passwordConfirm 추가, 입사일

--- CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/user/dto/UserUpdateRequestDto.java
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/user/dto/UserUpdateRequestDto.java
@@ -10,9 +10,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserUpdateRequestDto {
     @JsonProperty("userName")
     private String name;
@@ -35,11 +35,20 @@ public class UserUpdateRequestDto {
     @JsonProperty("userStatus")
     private Users.UserStatus status;
 
+    @JsonProperty("userPassword")
+    private String password;
+
+    @JsonProperty("userPasswordConfirm")
+    private String passwordConfirm;
+
     @JsonProperty("userAddress")
     private String address;
 
     @JsonProperty("userTerminationDate")
     private LocalDate terminationDate;
+
+    @JsonProperty("userHiredDate")
+    private LocalDate hiredDate;
 
     @JsonProperty("userExtension")
     private String extension;
