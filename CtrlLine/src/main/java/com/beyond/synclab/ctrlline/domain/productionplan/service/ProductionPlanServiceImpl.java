49d1e563588d166b4b055bf83ad546a583350b60
viroovr <tjgusdnjs789@gmail.com>
2025-11-15 13:38:27 +0900

    [Feat] ProductionPlan 생성 로직 설정

--- CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/productionplan/service/ProductionPlanServiceImpl.java
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/productionplan/service/ProductionPlanServiceImpl.java
@@ -1,10 +1,22 @@
 package com.beyond.synclab.ctrlline.domain.productionplan.service;
 
+import com.beyond.synclab.ctrlline.common.exception.AppException;
+import com.beyond.synclab.ctrlline.domain.factory.entity.Factories;
+import com.beyond.synclab.ctrlline.domain.factory.errorcode.FactoryErrorCode;
+import com.beyond.synclab.ctrlline.domain.factory.repository.FactoryRepository;
+import com.beyond.synclab.ctrlline.domain.item.entity.Items;
+import com.beyond.synclab.ctrlline.domain.item.exception.ItemErrorCode;
+import com.beyond.synclab.ctrlline.domain.item.repository.ItemRepository;
+import com.beyond.synclab.ctrlline.domain.line.entity.Lines;
+import com.beyond.synclab.ctrlline.domain.line.errorcode.LineErrorCode;
+import com.beyond.synclab.ctrlline.domain.line.repository.LineRepository;
 import com.beyond.synclab.ctrlline.domain.production.repository.ProductionPlanRepository;
 import com.beyond.synclab.ctrlline.domain.productionplan.dto.CreateProductionPlanRequestDto;
 import com.beyond.synclab.ctrlline.domain.productionplan.dto.ProductionPlanResponseDto;
+import com.beyond.synclab.ctrlline.domain.productionplan.entity.ProductionPlans;
 import com.beyond.synclab.ctrlline.domain.user.entity.Users;
-import com.beyond.synclab.ctrlline.domain.user.entity.Users.UserRole;
+import com.beyond.synclab.ctrlline.domain.user.errorcode.UserErrorCode;
+import com.beyond.synclab.ctrlline.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -16,16 +28,56 @@ import org.springframework.transaction.annotation.Transactional;
 public class ProductionPlanServiceImpl implements ProductionPlanService {
 
     private final ProductionPlanRepository productionPlanRepository;
+    private final UserRepository userRepository;
+    private final LineRepository lineRepository;
+    private final FactoryRepository factoryRepository;
+    private final ItemRepository itemRepository;
 
     @Override
     @Transactional
     public ProductionPlanResponseDto createProductionPlan(CreateProductionPlanRequestDto requestDto, Users user) {
 
+        Users salesManager = userRepository.findByEmpNo(requestDto.getSalesManagerNo())
+                .orElseThrow(() -> {
+                    log.debug("SalesManager 가 존재하지 않습니다.");
+                    return new AppException(UserErrorCode.USER_NOT_FOUND);
+                });
 
-        if (user.getRole().equals(UserRole.USER)) {
-            return null;
-        }
+        Users productionManager = userRepository.findByEmpNo(requestDto.getProductionManagerNo())
+                .orElseThrow(() -> {
+                    log.debug("ProductionManager 가 존재하지 않습니다.");
+                    return new AppException(UserErrorCode.USER_NOT_FOUND);
+                });
 
+        Lines line = lineRepository.findBylineCode(requestDto.getLineCode())
+                .orElseThrow(() -> {
+                    log.debug("Line 이 존재하지 않습니다.");
+                    return new AppException(LineErrorCode.LINE_NOT_FOUND);
+                });
+
+        Factories factory = factoryRepository.findByFactoryCode(requestDto.getFactoryCode())
+                .orElseThrow(() -> {
+                    log.debug("Factory 가 존재하지 않습니다.");
+                    return new AppException(FactoryErrorCode.FACTORY_NOT_FOUND);
+                });
+
+        Items item = itemRepository.findByItemCode(requestDto.getItemCode())
+                .orElseThrow(() -> {
+                    log.debug("Item 이 존재하지 않습니다.");
+                    return new AppException(ItemErrorCode.ITEM_NOT_FOUND);
+                });
+
+        String documentNo = createDocumentNo();
+
+        ProductionPlans productionPlan = requestDto.toEntity(salesManager, productionManager, line, documentNo);
+
+        productionPlanRepository.save(productionPlan);
+
+
+        return ProductionPlanResponseDto.fromEntity(productionPlan, factory, item);
+    }
+
+    private String createDocumentNo() {
         return null;
     }
 }
