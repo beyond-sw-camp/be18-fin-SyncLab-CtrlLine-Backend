4f1786b082e971d3db94f41e6d857ffd1ebc8e8e
dobbyrr <zxr1033@gmail.com>
2025-11-17 16:47:43 +0900

    [Feat]불량 등록 코드 수정

--- CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/telemetry/repository/DefectiveRepository.java
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/telemetry/repository/DefectiveRepository.java
@@ -1,14 +1,7 @@
 package com.beyond.synclab.ctrlline.domain.telemetry.repository;
 
 import com.beyond.synclab.ctrlline.domain.telemetry.entity.Defectives;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
-
-import jakarta.persistence.LockModeType;
 
 public interface DefectiveRepository extends JpaRepository<Defectives, Long> {
-
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    Optional<Defectives> findTopByDocumentNoStartingWithOrderByDocumentNoDesc(String documentNoPrefix);
 }
