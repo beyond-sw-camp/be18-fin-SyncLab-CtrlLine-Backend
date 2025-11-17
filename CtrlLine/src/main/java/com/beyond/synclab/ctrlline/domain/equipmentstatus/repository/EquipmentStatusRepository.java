d029a4c79b73e28c0114a1e453b345bf3fd2117d
inhwa1003 <iinhwa016@gmail.com>
2025-11-17 17:43:39 +0900

    [feat] SonarQube한테 0고백 1차임.

--- CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/equipmentstatus/repository/EquipmentStatusRepository.java
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/equipmentstatus/repository/EquipmentStatusRepository.java
@@ -1,6 +1,5 @@
 package com.beyond.synclab.ctrlline.domain.equipmentstatus.repository;
 
-import com.beyond.synclab.ctrlline.domain.equipment.entity.Equipments;
 import com.beyond.synclab.ctrlline.domain.equipmentstatus.entity.EquipmentStatuses;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
