480eedbc23edd395c2d8345fcfd748197dde3fe4
viroovr <tjgusdnjs789@gmail.com>
2025-11-17 17:47:13 +0900

    [Feat] 라인 에러코드 추가

--- CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/line/errorcode/LineErrorCode.java
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/line/errorcode/LineErrorCode.java
@@ -10,7 +10,11 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum LineErrorCode implements ErrorCode {
     // 404
-    LINE_NOT_FOUND(HttpStatus.NOT_FOUND, "LINE_NOT_FOUND", "라인을 찾을 수 없습니다.");
+    LINE_NOT_FOUND(HttpStatus.NOT_FOUND, "LINE_NOT_FOUND", "라인을 찾을 수 없습니다."),
+    NO_EQUIPMENT_FOUND(HttpStatus.NOT_FOUND, "NO_EQUIPMENT_FOUND", "라인에 해당하는 설비를 찾을 수 없습니다."),
+
+    // 500
+    INVALID_EQUIPMENT_PPM(HttpStatus.INTERNAL_SERVER_ERROR, "INVALID_EQUIPMENT_PPM", "유효 설비 PPM계산이 불가합니다.");
 
     private final HttpStatus status;
     private final String code;
