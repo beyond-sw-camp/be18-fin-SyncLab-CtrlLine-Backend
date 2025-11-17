b42fbad5b8afb6959e0bb3d0fca1b58674d2d4c8
viroovr <tjgusdnjs789@gmail.com>
2025-11-17 10:51:12 +0900

    [Feat] 전표번호 기반으로 prefix 조회 레포메소드 작성

--- CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/production/repository/ProductionPlanRepository.java
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/production/repository/ProductionPlanRepository.java
@@ -2,9 +2,13 @@ package com.beyond.synclab.ctrlline.domain.production.repository;
 
 import com.beyond.synclab.ctrlline.domain.productionplan.entity.ProductionPlans;
 import com.beyond.synclab.ctrlline.domain.productionplan.entity.ProductionPlans.PlanStatus;
+import jakarta.persistence.LockModeType;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -13,4 +17,8 @@ public interface ProductionPlanRepository extends JpaRepository<ProductionPlans,
     List<ProductionPlans> findAllByStatusAndStartTimeLessThanEqual(PlanStatus status, LocalDateTime startTime);
 
     List<ProductionPlans> findAllByLine_lineCode(String lineCode);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT pp.documentNo FROM ProductionPlans pp WHERE pp.documentNo LIKE :prefix% ORDER BY pp.documentNo DESC")
+    List<String> findByDocumentNoByPrefix(@Param("prefix") String prefix);
 }
