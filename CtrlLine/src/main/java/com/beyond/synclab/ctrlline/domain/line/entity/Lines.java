106c75644df394033650f217b3422d13f3404d2d
kimeodml <kimeodml@gmail.com>
2025-11-18 12:36:25 +0900

    [Feat] 라인 상세 조회, 목록 조회 구

--- CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/line/entity/Lines.java
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/line/entity/Lines.java
@@ -1,12 +1,18 @@
 package com.beyond.synclab.ctrlline.domain.line.entity;
 
+import com.beyond.synclab.ctrlline.domain.factory.entity.Factories;
 import com.beyond.synclab.ctrlline.domain.log.util.EntityActionLogger;
 
+import com.beyond.synclab.ctrlline.domain.user.entity.Users;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -14,7 +20,14 @@ import java.time.LocalDateTime;
 @Getter
 @Entity
 @Builder
-@Table(name = "line")
+@Table(
+        name = "line",
+        uniqueConstraints = {
+        @UniqueConstraint(
+                name = "uq_line_code",
+                columnNames = "line_code"
+        )
+})
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @EntityListeners(EntityActionLogger.class)
@@ -25,14 +38,22 @@ public class Lines {
     @Column(name = "line_id")
     private Long id;
 
-    @Column(name = "factory_id", nullable = false)
-    private Long factoryId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "factory_id", nullable = false)
+    private Factories factory;
 
-    @Column(name = "line_code", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private Users user;
+
+    @Column(name = "line_code", nullable = false, unique = true)
     private String lineCode;
 
     @Column(name = "line_name", nullable = false, length = 100)
     private String lineName;
+
+    @Column(name = "is_active", nullable = false)
+    private Boolean isActive;
 
     @Column(name = "created_at", nullable = false,
             columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
@@ -42,16 +63,16 @@ public class Lines {
             columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
     private LocalDateTime updatedAt;
 
-    public Lines(Long id, Long factoryId, String lineCode) {
+    public Lines(Long id, Factories factory, String lineCode) {
         this.id = id;
-        this.factoryId = factoryId;
+        this.factory = factory;
         this.lineCode = lineCode;
         this.createdAt = LocalDateTime.now();
         this.updatedAt = LocalDateTime.now();
         this.lineName = "";
     }
 
-    public static Lines of(Long id, Long factoryId, String lineCode) {
-        return new Lines(id, factoryId, lineCode);
+    public static Lines of(Long id, Factories factory, String lineCode) {
+        return new Lines(id, factory, lineCode);
     }
 }
