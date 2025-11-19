0911d6625347e9372e582ca3740b31dbc075c170
viroovr <tjgusdnjs789@gmail.com>
2025-11-19 12:14:57 +0900

    [Feat] 조회 param command와 응답 dto

---
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/productionplan/dto/ProductionPlanListResponseDto.java
@@ -0,0 +1,42 @@
+package com.beyond.synclab.ctrlline.domain.productionplan.dto;
+
+import com.beyond.synclab.ctrlline.domain.productionplan.entity.ProductionPlans;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProductionPlanListResponseDto {
+    private Long id;
+    private String documentNo;
+    private ProductionPlans.PlanStatus status;
+    private String factoryName;
+    private String salesManagerName;
+    private String productionManagerName;
+    private String itemName;
+    private BigDecimal plannedQty;
+    private LocalDate dueDate;
+    private String remark;
+
+    public static ProductionPlanListResponseDto fromEntity(ProductionPlans productionPlans) {
+        return ProductionPlanListResponseDto.builder()
+            .id(productionPlans.getId())
+            .documentNo(productionPlans.getDocumentNo())
+            .status(productionPlans.getStatus())
+            .factoryName(productionPlans.getItemLine().getLine().getFactory().getFactoryName())
+            .salesManagerName(productionPlans.getSalesManager().getName())
+            .productionManagerName(productionPlans.getProductionManager().getName())
+            .itemName(productionPlans.getItemLine().getItem().getItemName())
+            .plannedQty(productionPlans.getPlannedQty())
+            .dueDate(productionPlans.getDueDate())
+            .remark(productionPlans.getRemark())
+            .build();
+    }
+}
