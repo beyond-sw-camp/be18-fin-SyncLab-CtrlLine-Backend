cd8bc3a8e5322bfe65d351538be3dc2113ca1543
viroovr <tjgusdnjs789@gmail.com>
2025-11-19 12:15:28 +0900

    [Feat] Plan Specification 등록

---
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/productionplan/spec/PlanSpecification.java
@@ -0,0 +1,94 @@
+package com.beyond.synclab.ctrlline.domain.productionplan.spec;
+
+import com.beyond.synclab.ctrlline.domain.factory.entity.Factories;
+import com.beyond.synclab.ctrlline.domain.item.entity.Items;
+import com.beyond.synclab.ctrlline.domain.itemline.entity.ItemsLines;
+import com.beyond.synclab.ctrlline.domain.line.entity.Lines;
+import com.beyond.synclab.ctrlline.domain.productionplan.entity.ProductionPlans;
+import com.beyond.synclab.ctrlline.domain.productionplan.entity.ProductionPlans.PlanStatus;
+import com.beyond.synclab.ctrlline.domain.user.entity.Users;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import lombok.experimental.UtilityClass;
+import org.springframework.data.jpa.domain.Specification;
+
+@UtilityClass
+public class PlanSpecification {
+
+    public Specification<ProductionPlans> planStatusEquals(PlanStatus planStatus) {
+        return (root, query, cb) ->
+            planStatus == null
+                ? null
+                : cb.equal(root.get("status"), planStatus);
+    }
+
+    public Specification<ProductionPlans> planFactoryNameContains(String factoryName) {
+        return (root, query, cb) -> {
+            if (factoryName == null)
+                return null;
+
+            // plan -> itemLine -> line -> factory
+            Join<ProductionPlans, ItemsLines> itemLine = root.join("itemLine", JoinType.LEFT);
+            Join<ItemsLines, Lines> line = itemLine.join("line", JoinType.LEFT);
+            Join<Lines, Factories> factory = line.join("factory", JoinType.LEFT);
+
+            return cb.equal(factory.get("factoryName"), factoryName);
+        };
+    }
+
+    public Specification<ProductionPlans> planItemNameContains(String itemName) {
+        return (root, query, cb) -> {
+            if (itemName == null)
+                return null;
+
+            // plan -> itemLine -> item
+            Join<ProductionPlans, ItemsLines> itemLine = root.join("itemLine", JoinType.LEFT);
+            Join<ItemsLines, Items> item = itemLine.join("item", JoinType.LEFT);
+
+            return cb.like(item.get("itemName"), "%" + itemName + "%");
+        };
+    }
+
+    public Specification<ProductionPlans> planSalesManagerNameContains(String name) {
+        return (root, query, cb) -> {
+            if (name == null)
+                return null;
+
+            Join<ProductionPlans, Users> pm = root.join("salesManager", JoinType.LEFT);
+            return cb.like(pm.get("name"), "%" + name + "%");
+        };
+    }
+
+    public Specification<ProductionPlans> planProductionManagerNameContains(String name) {
+        return (root, query, cb) -> {
+            if (name == null)
+                return null;
+
+            Join<ProductionPlans, Users> pm = root.join("productionManager", JoinType.LEFT);
+            return cb.like(pm.get("name"), "%" + name + "%");
+        };
+    }
+
+    public Specification<ProductionPlans> planDueDateBefore(LocalDate dueDate) {
+        return (root, query, cb) -> {
+            if (dueDate == null) return null;
+            return cb.lessThanOrEqualTo(root.get("dueDate"), dueDate);
+        };
+    }
+
+    public Specification<ProductionPlans> planStartTimeAfter(LocalDateTime start) {
+        return (root, query, cb) -> {
+            if (start == null) return null;
+            return cb.greaterThanOrEqualTo(root.get("startTime"), start);
+        };
+    }
+
+    public Specification<ProductionPlans> planEndTimeBefore(LocalDateTime end) {
+        return (root, query, cb) -> {
+            if (end == null) return null;
+            return cb.lessThanOrEqualTo(root.get("endTime"), end);
+        };
+    }
+}
