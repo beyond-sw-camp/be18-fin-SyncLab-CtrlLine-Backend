a8b596b5dd3f00fa65240844ebdc9d1095580253
viroovr <tjgusdnjs789@gmail.com>
2025-11-20 12:11:13 +0900

    [Refactor] 생산계획 수정 request dto 추가

---
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/productionplan/dto/UpdateProductionPlanRequestDto.java
@@ -0,0 +1,27 @@
+package com.beyond.synclab.ctrlline.domain.productionplan.dto;
+
+import com.beyond.synclab.ctrlline.domain.productionplan.entity.ProductionPlans.PlanStatus;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UpdateProductionPlanRequestDto {
+    private PlanStatus status;
+    private String salesManagerNo;
+    private String productionManagerNo;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private String remark;
+    private String factoryCode;
+    private String itemCode;
+    private String lineCode;
+    private LocalDate dueDate;
+}
