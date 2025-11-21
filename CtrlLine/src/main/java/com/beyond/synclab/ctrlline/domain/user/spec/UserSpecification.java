61a4282029f6aa362616931a6d389f4ed741e165
viroovr <tjgusdnjs789@gmail.com>
2025-11-21 18:48:05 +0900

    [Feat] 유저이름, 사번으로 조회 가능하도록 수정

--- CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/user/spec/UserSpecification.java
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/user/spec/UserSpecification.java
@@ -5,62 +5,75 @@ import com.beyond.synclab.ctrlline.domain.user.entity.Users.UserPosition;
 import com.beyond.synclab.ctrlline.domain.user.entity.Users.UserRole;
 import com.beyond.synclab.ctrlline.domain.user.entity.Users.UserStatus;
 import java.time.LocalDate;
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
+import lombok.experimental.UtilityClass;
 import org.springframework.data.jpa.domain.Specification;
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@UtilityClass
 public class UserSpecification {
-    public static Specification<Users> userDepartmentEquals(String dept) {
+    public Specification<Users> userDepartmentEquals(String dept) {
         return (root, query, cb) ->
             dept == null || dept.isEmpty()
                 ? null
                 : cb.equal(root.get("department"), dept);
     }
 
-    public static Specification<Users> userStatusEquals(UserStatus status) {
+    public Specification<Users> userStatusEquals(UserStatus status) {
         return (root, query, cb) ->
             status == null
                 ? null
                 : cb.equal(root.get("status"), status);
     }
 
-    public static Specification<Users> userRoleEquals(UserRole role) {
+    public Specification<Users> userRoleEquals(UserRole role) {
         return (root, query, cb) ->
             role == null
                 ? null
                 : cb.equal(root.get("role"), role);
     }
 
-    public static Specification<Users> userPositionEquals(UserPosition userPosition) {
+    public Specification<Users> userPositionEquals(UserPosition userPosition) {
         return (root, query, cb) ->
             userPosition == null
                 ? null
                 : cb.equal(root.get("position"), userPosition);
     }
 
-    public static Specification<Users> userPhoneNumberContains(String phone) {
+    public Specification<Users> userPhoneNumberContains(String phone) {
         return (root, query, cb) ->
             phone == null || phone.isEmpty()
                 ? null
                 : cb.like(root.get("phoneNumber"), "%" + phone + "%");
     }
 
-    public static Specification<Users> userEmailContains(String email) {
+    public Specification<Users> userEmpNoContains(String empNo) {
         return (root, query, cb) ->
-            email == null || email.isEmpty()
+            empNo == null || empNo.isBlank()
+                ? null
+                : cb.like(root.get("empNO"), "%" + empNo + "%");
+    }
+
+    public Specification<Users> userNameContains(String name) {
+        return (root, query, cb) ->
+            name == null || name.isBlank()
+                ? null
+                : cb.like(root.get("name"), "%" + name + "%");
+    }
+
+    public Specification<Users> userEmailContains(String email) {
+        return (root, query, cb) ->
+            email == null || email.isBlank()
             ? null
             : cb.like(root.get("email"), "%" + email + "%");
     }
 
-    public static Specification<Users> userHiredDateAfter(LocalDate date) {
+    public Specification<Users> userHiredDateAfter(LocalDate date) {
         return (root, query, cb) ->
             date == null
                 ? null
                 : cb.greaterThanOrEqualTo(root.get("hiredDate"), date);
     }
 
-    public static Specification<Users> userTerminationDateBefore(LocalDate date) {
+    public Specification<Users> userTerminationDateBefore(LocalDate date) {
         return (root, query, cb) ->
             date == null
                 ? null
