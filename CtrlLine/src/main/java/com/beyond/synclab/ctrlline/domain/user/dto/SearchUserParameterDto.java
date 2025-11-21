de01cc7034eb6064d8d8a48a53258d46b211501f
viroovr <tjgusdnjs789@gmail.com>
2025-11-21 18:42:14 +0900

    [Feat] 사번과 이름 추가

--- CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/user/dto/SearchUserParameterDto.java
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/user/dto/SearchUserParameterDto.java
@@ -6,13 +6,15 @@ import com.beyond.synclab.ctrlline.domain.user.entity.Users.UserStatus;
 import java.time.LocalDate;
 import org.springframework.format.annotation.DateTimeFormat;
 
-public record UserSearchCommand(
+public record SearchUserParameterDto(
     String userDepartment,
     UserStatus userStatus,
     UserRole userRole,
     UserPosition userPosition,
     String userPhoneNumber,
     String userEmail,
+    String userEmpNo,
+    String userName,
 
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     LocalDate hiredDate,
