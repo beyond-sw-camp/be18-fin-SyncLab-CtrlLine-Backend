d9171af13712f192ebc3e432252acdc7c24e3216
dobbyrr <zxr1033@gmail.com>
2025-11-24 19:54:04 +0900

    [Feat] Lot 엔티티

---
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/lot/entity/Lots.java
@@ -0,0 +1,49 @@
+package com.beyond.synclab.ctrlline.domain.lot.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Getter
+@Builder
+@Entity
+@Table(name = "lot")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@EqualsAndHashCode(of = "id")
+public class Lot {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "lot_id")
+    private Long id;
+
+    @Column(name = "item_id", nullable = false)
+    private Long itemId;
+
+    @Column(name = "production_plan_id", nullable = false)
+    private Long productionPlanId;
+
+    @Column(name = "lot_no", nullable = false, length = 32)
+    private String lotNo;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
