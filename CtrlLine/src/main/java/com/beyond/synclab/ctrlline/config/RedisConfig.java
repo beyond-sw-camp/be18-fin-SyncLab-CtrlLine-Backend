a7821fafabce3a246beaf40f3273fe5b48aa7daa
viroovr <tjgusdnjs789@gmail.com>
2025-11-24 16:03:15 +0900

    [Feat] 레디스용 CacheManager 추가

--- CtrlLine/src/main/java/com/beyond/synclab/ctrlline/config/RedisConfig.java
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/config/RedisConfig.java
@@ -1,12 +1,22 @@
 package com.beyond.synclab.ctrlline.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.time.Duration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@EnableCaching
 public class RedisConfig {
     @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
@@ -18,5 +28,26 @@ public class RedisConfig {
         template.setValueSerializer(new StringRedisSerializer());
 
         return template;
+    }
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(
+            new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+        );
+
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+            .entryTtl(Duration.ofMinutes(10)) // 기본 TTL
+            .serializeValuesWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(serializer)
+            )
+            .disableCachingNullValues();       // null 값 캐싱 방지
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+            .cacheDefaults(config)
+            .transactionAware()
+            .build();
     }
 }
