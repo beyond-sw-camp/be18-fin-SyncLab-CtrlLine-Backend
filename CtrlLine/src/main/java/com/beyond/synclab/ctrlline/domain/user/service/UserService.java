ea26797b4dbf94a0145ca255b610635decdeab0d
서현원 <109474853+viroovr@users.noreply.github.com>
2025-11-24 12:34:19 +0900

    [Feat] 사용자 목록 조회시 필터링 조건 추가 (#148)
    * [Feat] 사번과 이름 추가
    
    * [Refactor] Dto명 변경
    
    * [Refactor] Dto명 변경 및 Builder 패턴
    
    * [Feat] 유저이름, 사번으로 조회 가능하도록 수정
    
    * [Refactor] builder 패턴 가능하게 수정
    
    * [Fix] empNo 오타 수정

--- CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/user/service/UserService.java
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/user/service/UserService.java
@@ -3,14 +3,14 @@ package com.beyond.synclab.ctrlline.domain.user.service;
 import com.beyond.synclab.ctrlline.domain.user.dto.UserListResponseDto;
 import com.beyond.synclab.ctrlline.domain.user.dto.UserUpdateMeRequestDto;
 import com.beyond.synclab.ctrlline.domain.user.dto.UserResponseDto;
-import com.beyond.synclab.ctrlline.domain.user.dto.UserSearchCommand;
+import com.beyond.synclab.ctrlline.domain.user.dto.SearchUserParameterDto;
 import com.beyond.synclab.ctrlline.domain.user.dto.UserUpdateRequestDto;
 import com.beyond.synclab.ctrlline.domain.user.entity.Users;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface UserService {
-    Page<UserListResponseDto> getUserList(UserSearchCommand command, Pageable pageable);
+    Page<UserListResponseDto> getUserList(SearchUserParameterDto command, Pageable pageable);
 
     UserResponseDto getUserById(Long userId);
 
