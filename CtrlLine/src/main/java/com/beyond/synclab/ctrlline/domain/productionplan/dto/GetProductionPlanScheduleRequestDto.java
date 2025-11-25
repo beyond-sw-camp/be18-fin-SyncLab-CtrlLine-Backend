7d5c7106aa5eb1e110fbf8443d49d09d9e118ce0
서현원 <109474853+viroovr@users.noreply.github.com>
2025-11-25 12:10:28 +0900

    [Feat] 생산계획 일정 조회 (#156)
    * [Test] api, 서비스 로직 테스트 코드 작성
    
    * [Feat] 컨트롤러 코드 작성
    
    * [Feat] 서비스 로직 작성
    
    * [Feat] 라인이름 조회, 아이템 코드 조회 조건 추가
    
    * [Refactor] 사번명 "emp" 제거
    
    * [Test] 일정 조회 테스트 코드 추가
    
    * [Feat] 레디스용 CacheManager 추가
    
    * [Feat] 생산계획 일정 조회할때 Key 생성 유틸 로직 추가
    
    * [Feat] 생산계획 일정 조회 컨트롤러 코드
    
    * [Feat] DTO 추가
    
    * [Feat] 일정 조회 로직 추가

---
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/productionplan/dto/GetProductionPlanScheduleRequestDto.java
@@ -0,0 +1,14 @@
+package com.beyond.synclab.ctrlline.domain.productionplan.dto;
+
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import lombok.Builder;
+
+@Builder
+public record GetProductionPlanScheduleRequestDto(
+    @NotNull LocalDateTime startTime,
+    @NotNull LocalDateTime endTime,
+    String factoryName,
+    String lineName
+) {
+}
