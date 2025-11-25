8d1d771c7a8d0b7b760deba6491a7116a3a888c7
서현원 <109474853+viroovr@users.noreply.github.com>
2025-11-25 12:10:28 +0900

    [Feat] 생산계획 일정 조회 (#156)
    * [Test] api, 서비스 로직 테스트 코드 작성
    
    * [Feat] 컨트롤러 코드 작성
    
    * [Feat] 서비스 로직 작성
    
    * [Feat] 라인이름 조회, 아이템 코드 조회 조건 추가
    
    * [Refactor] 사번명 "emp" 제거
    
    * [Test] 일정 조회 테스트 코드 추가
    
    * [Feat] 레디스용 CacheManager 추가
    
    * [Feat] 생산계획 일정 조회할때 Key 생성 유틸 로직 추가
    
    * [Feat] 생산계획 일정 조회 컨트롤러 코드
    
    * [Feat] DTO 추가
    
    * [Feat] 일정 조회 로직 추가

---
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/productionplan/dto/GetProductionPlanScheduleResponseDto.java
@@ -0,0 +1,52 @@
+package com.beyond.synclab.ctrlline.domain.productionplan.dto;
+
+import com.beyond.synclab.ctrlline.domain.productionplan.entity.ProductionPlans;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class GetProductionPlanScheduleResponseDto {
+    private Long id;
+    private String lineCode;
+    private String lineName;
+    private String factoryCode;
+    private String factoryName;
+    private String salesManagerNo;
+    private String productionManagerNo;
+    private String documentNo;
+    private String itemCode;
+    private ProductionPlans.PlanStatus status;
+    private LocalDate dueDate;
+    private BigDecimal plannedQty;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private String remark;
+
+    public static GetProductionPlanScheduleResponseDto fromEntity(ProductionPlans pp) {
+        return GetProductionPlanScheduleResponseDto.builder()
+            .id(pp.getId())
+            .lineCode(pp.getItemLine().getLine().getLineCode())
+            .lineName(pp.getItemLine().getLine().getLineName())
+            .factoryCode(pp.getItemLine().getLine().getFactory().getFactoryCode())
+            .factoryName(pp.getItemLine().getLine().getFactory().getFactoryName())
+            .salesManagerNo(pp.getSalesManager().getEmpNo())
+            .productionManagerNo(pp.getProductionManager().getEmpNo())
+            .documentNo(pp.getDocumentNo())
+            .status(pp.getStatus())
+            .dueDate(pp.getDueDate())
+            .plannedQty(pp.getPlannedQty())
+            .startTime(pp.getStartTime())
+            .endTime(pp.getEndTime())
+            .remark(pp.getRemark())
+            .build();
+    }
+}
