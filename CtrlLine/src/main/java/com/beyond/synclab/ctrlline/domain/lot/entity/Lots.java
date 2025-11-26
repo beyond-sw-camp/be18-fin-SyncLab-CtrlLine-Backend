724531ae5ba13144d36713f65fbe84519b7cc1b2
dobbyrr <zxr1033@gmail.com>
2025-11-26 10:37:40 +0900

    [Refactor] 로트 엔티티 FK 값 별도 필드로 명시

--- CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/lot/entity/Lots.java
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/lot/entity/Lots.java
@@ -1,10 +1,15 @@
 package com.beyond.synclab.ctrlline.domain.lot.entity;
 
+import com.beyond.synclab.ctrlline.domain.item.entity.Items;
+import com.beyond.synclab.ctrlline.domain.productionplan.entity.ProductionPlans;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
@@ -37,8 +42,16 @@ public class Lots {
     @Column(name = "item_id", nullable = false)
     private Long itemId;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id", insertable = false, updatable = false)
+    private Items item;
+
     @Column(name = "production_plan_id", nullable = false)
     private Long productionPlanId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "production_plan_id", insertable = false, updatable = false)
+    private ProductionPlans productionPlan;
 
     @Column(name = "lot_no", nullable = false, length = 32)
     private String lotNo;
