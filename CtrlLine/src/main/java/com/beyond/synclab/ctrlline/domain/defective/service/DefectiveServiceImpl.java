3fddd3831628d1c0643f21542ed3d2c5da3c8eca
viroovr <tjgusdnjs789@gmail.com>
2025-11-26 10:47:26 +0900

    [Feat] 상세조회 서비스 로직 추가

--- CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/defective/service/DefectiveServiceImpl.java
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/defective/service/DefectiveServiceImpl.java
@@ -1,9 +1,13 @@
 package com.beyond.synclab.ctrlline.domain.defective.service;
 
+import com.beyond.synclab.ctrlline.common.exception.AppException;
 import com.beyond.synclab.ctrlline.domain.defective.dto.GetDefectiveDetailResponseDto;
-import com.beyond.synclab.ctrlline.domain.itemline.repository.ItemLineRepository;
-import com.beyond.synclab.ctrlline.domain.line.repository.LineRepository;
-import com.beyond.synclab.ctrlline.domain.telemetry.repository.DefectiveRepository;
+import com.beyond.synclab.ctrlline.domain.defective.errorcode.DefectiveErrorCode;
+import com.beyond.synclab.ctrlline.domain.productionplan.entity.PlanDefectiveXrefs;
+import com.beyond.synclab.ctrlline.domain.productionplan.entity.PlanDefectives;
+import com.beyond.synclab.ctrlline.domain.productionplan.repository.PlanDefectiveRepository;
+import com.beyond.synclab.ctrlline.domain.productionplan.repository.PlanDefectiveXrefRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,14 +18,17 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class DefectiveServiceImpl implements DefectiveService {
 
-    private final DefectiveRepository defectiveRepository;
-    private final LineRepository lineRepository;
-    private final ItemLineRepository itemLineRepository;
-
+    private final PlanDefectiveXrefRepository planDefectiveXrefRepository;
+    private final PlanDefectiveRepository planDefectiveRepository;
 
     @Override
     @Transactional(readOnly = true)
     public GetDefectiveDetailResponseDto getDefective(String documentNo) {
-        return null;
+        PlanDefectives planDefectives = planDefectiveRepository.findByDefectiveDocumentNo(documentNo)
+            .orElseThrow(() -> new AppException(DefectiveErrorCode.PLAN_DEFECTIVE_NOT_FOUND));
+
+        List<PlanDefectiveXrefs> planDefectiveXrefList = planDefectiveXrefRepository.findAllByPlanDefectiveId(planDefectives.getId());
+
+        return GetDefectiveDetailResponseDto.fromEntity(planDefectives, planDefectiveXrefList);
     }
 }
