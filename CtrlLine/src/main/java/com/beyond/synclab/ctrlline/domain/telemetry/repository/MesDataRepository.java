346fcffa9324929b23b48e0f9c3de6751e2f45e8
dobbyrr <zxr1033@gmail.com>
2025-11-27 14:24:51 +0900

    [Feat] Raw Data DB factory_id 추가

--- CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/telemetry/repository/MesDataRepository.java
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/telemetry/repository/MesDataRepository.java
@@ -1,7 +1,9 @@
 package com.beyond.synclab.ctrlline.domain.telemetry.repository;
 
 import com.beyond.synclab.ctrlline.domain.telemetry.entity.MesDatas;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MesDataRepository extends JpaRepository<MesDatas, Long> {
+    Optional<MesDatas> findFirstByFactoryIdOrderByCreatedAtDesc(Long factoryId);
 }
