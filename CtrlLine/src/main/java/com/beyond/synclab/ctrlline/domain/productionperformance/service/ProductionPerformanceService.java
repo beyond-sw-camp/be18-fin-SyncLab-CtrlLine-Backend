3dae2e3ffda10a9366b96d9d7923e69c159fd6fb
KorSwib <mins924@naver.com>
2025-11-27 09:24:20 +0900

    [Feat] 생산실적 공장별 월별 6개월 누적 생산량 조회 구현 완료. (#172)
    Co-authored-by: KorSwib <symobile924@gmail.com>

--- CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/productionperformance/service/ProductionPerformanceService.java
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/productionperformance/service/ProductionPerformanceService.java
@@ -5,6 +5,7 @@ import com.beyond.synclab.ctrlline.domain.productionperformance.dto.request.Sear
 import com.beyond.synclab.ctrlline.domain.productionperformance.dto.response.GetAllProductionPerformanceResponseDto;
 import com.beyond.synclab.ctrlline.domain.productionperformance.dto.response.GetProductionPerformanceDetailResponseDto;
 import com.beyond.synclab.ctrlline.domain.productionperformance.dto.response.GetProductionPerformanceListResponseDto;
+import com.beyond.synclab.ctrlline.domain.productionperformance.dto.response.GetProductionPerformanceMonthlySumResponseDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -19,10 +20,16 @@ public interface ProductionPerformanceService {
     );
 
     // 생산실적 상세 조회
-    GetProductionPerformanceDetailResponseDto getProductionPerformanceDetail(Long id);
+    GetProductionPerformanceDetailResponseDto getProductionPerformanceDetail(
+            Long id
+    );
 
     // 생산실적 현황 조회
     List<GetAllProductionPerformanceResponseDto> getAllProductionPerformances(
             SearchAllProductionPerformanceRequestDto condition
     );
+
+    // 공장별 월별 생산량 조회 (대시보드)
+    GetProductionPerformanceMonthlySumResponseDto.FactoryMonthlyPerformance
+    getMonthlySumProductionPerformances(String factoryCode, String baseMonth);
 }
