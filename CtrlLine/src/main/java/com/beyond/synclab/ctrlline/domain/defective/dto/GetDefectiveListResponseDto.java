452049ad73ed93d3ed062f551a453f59f8243496
viroovr <tjgusdnjs789@gmail.com>
2025-11-28 12:08:03 +0900

    [Feat] 목록조회 서비스 코드 작성

--- CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/defective/dto/GetDefectiveListResponseDto.java
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/defective/dto/GetDefectiveListResponseDto.java
@@ -1,5 +1,9 @@
 package com.beyond.synclab.ctrlline.domain.defective.dto;
 
+import com.beyond.synclab.ctrlline.domain.item.entity.Items;
+import com.beyond.synclab.ctrlline.domain.line.entity.Lines;
+import com.beyond.synclab.ctrlline.domain.productionperformance.entity.ProductionPerformances;
+import com.beyond.synclab.ctrlline.domain.productionplan.entity.PlanDefectives;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
@@ -9,9 +13,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
 @Builder(toBuilder = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class GetDefectiveListResponseDto {
     private Long planDefectiveId;
     private String defectiveDocNo;
@@ -22,7 +26,27 @@ public class GetDefectiveListResponseDto {
     private String lineCode;
     private String lineName;
     private BigDecimal defectiveTotalQty;
-    private Double defectiveTotalRate;
+    private BigDecimal defectiveTotalRate;
     private String productionPerformanceDocNo;
     private LocalDateTime createdAt;
+
+    public static GetDefectiveListResponseDto fromEntity(PlanDefectives px, ProductionPerformances perf) {
+        Items item = px.getProductionPlan().getItemLine().getItem();
+        Lines line = px.getProductionPlan().getItemLine().getLine();
+
+        return GetDefectiveListResponseDto.builder()
+            .planDefectiveId(px.getId())
+            .defectiveDocNo(px.getDefectiveDocumentNo())
+            .itemId(item.getId())
+            .itemCode(item.getItemCode())
+            .itemName(item.getItemName())
+            .lineId(line.getId())
+            .lineCode(line.getLineCode())
+            .lineName(line.getLineName())
+            .defectiveTotalQty(perf.getPerformanceDefectiveQty())
+            .defectiveTotalRate(perf.getPerformanceDefectiveRate())
+            .productionPerformanceDocNo(perf.getPerformanceDocumentNo())
+            .createdAt(px.getCreatedAt())
+            .build();
+    }
 }
