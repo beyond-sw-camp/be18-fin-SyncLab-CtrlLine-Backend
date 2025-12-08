ffdce3c3877bd12288bcc50e0e42987c40b5a8fc
viroovr <tjgusdnjs789@gmail.com>
2025-12-08 17:13:24 +0900

    [Feat] gzip 읽기 확인

--- CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/serial/storage/LocalSerialStorageService.java
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/serial/storage/LocalSerialStorageService.java
@@ -1,6 +1,9 @@
 package com.beyond.synclab.ctrlline.domain.serial.storage;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.AccessDeniedException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,6 +12,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
+import java.util.zip.GZIPInputStream;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.StringUtils;
 
@@ -67,10 +71,15 @@ public class LocalSerialStorageService implements SerialStorageService {
 
     @Override
     public List<String> read(String path) {
-        try {
-            return Files.readAllLines(Path.of(path));
+        Path filePath = Path.of(path);
+
+        try (GZIPInputStream gzipInputStream = new GZIPInputStream(Files.newInputStream(filePath));
+            InputStreamReader reader = new InputStreamReader(gzipInputStream, StandardCharsets.UTF_8);
+            BufferedReader bufferedReader = new BufferedReader(reader)) {
+
+            return bufferedReader.lines().toList();
         } catch (IOException ex) {
-            throw new IllegalStateException("Failed to read local serial file: " + path, ex);
+            throw new IllegalStateException("Failed to read gzip serial file: " + path, ex);
         }
     }
 }
