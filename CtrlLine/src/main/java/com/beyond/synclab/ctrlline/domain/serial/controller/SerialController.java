7ed9098fe3eea7e6033b5e5ff0abbd5548875ee8
서현원 <109474853+viroovr@users.noreply.github.com>
2025-12-08 17:58:54 +0900

    [Feat] serial 번호 조회 (#284)
    * [Feat] gzip 읽기 확인
    
    * [Feat] 반환형태 통합
    
    * [Feat] 덮어씌기 형태로 변경

--- CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/serial/controller/SerialController.java
+++ CtrlLine/src/main/java/com/beyond/synclab/ctrlline/domain/serial/controller/SerialController.java
@@ -1,8 +1,10 @@
 package com.beyond.synclab.ctrlline.domain.serial.controller;
 
+import com.beyond.synclab.ctrlline.common.dto.BaseResponse;
 import com.beyond.synclab.ctrlline.domain.serial.dto.response.GetLotSerialListResponseDto;
 import com.beyond.synclab.ctrlline.domain.serial.service.SerialService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,9 +15,11 @@ public class SerialController {
     private final SerialService serialService;
 
     @GetMapping("/{lotId}/serials")
-    public GetLotSerialListResponseDto getSerialList(
+    public ResponseEntity<BaseResponse<GetLotSerialListResponseDto>> getSerialList(
             @PathVariable Long lotId
     ) {
-        return serialService.getSerialListByLotId(lotId);
+        GetLotSerialListResponseDto responseDto =  serialService.getSerialListByLotId(lotId);
+
+        return ResponseEntity.ok(BaseResponse.ok(responseDto));
     }
 }
